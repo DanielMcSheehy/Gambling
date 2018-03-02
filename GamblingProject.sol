3e4a4cec4f440651a79ccb94c79662894ef3cb5a
Daniel McSheehy <dsm140130@utdallas.edu>
2018-03-02 13:55:28 -0600

    Update GamblingProject.sol

--- GamblingProject.sol
+++ GamblingProject.sol
@@ -15,23 +15,27 @@ contract GambleTool {
   Bet[] public bets; 
   mapping (uint => address) public betToOwner;
 
-  event gameStarted(uint index, uint maxPlayers);
-  event joined(uint sentValue, uint balance);
-  event win(uint winner, uint amount);
+  event newGame(uint index, uint maxPlayers, uint betValue);
+  event joined(uint index, address playerAddress, uint balance);
+  event win(uint index, address winnerAddress, uint payout);
 
   function setBet(uint betValue, uint maxPlayers) public { // This wont be public 
     require(msg.sender == owner); //Might not be the best place
     address[] memory blank;
     uint index = bets.push(Bet(blank, address(0), uint8(betValue), uint8(maxPlayers), true)) -1;
     betToOwner[index] = msg.sender; // a little confusing 
-    gameStarted(index, maxPlayers);
+    newGame(index, maxPlayers, betValue);
   }
 
   function join(uint index) public payable {
     require(bets[index].players.length < bets[index]._maxPlayers); //Check for open
-    if (msg.value >= bets[index]._betValue) { // Require? 
-      bets[index].players.push(msg.sender);
-      joined(msg.value, this.balance);
+    require(msg.value == bets[index]._betValue);
+    
+    bets[index].players.push(msg.sender);
+    joined(index, msg.sender, this.balance);
+    
+    if (bets[index].players.length == bets[index]._maxPlayers) {
+      startGame(index);
     }
   }
 
@@ -40,17 +44,22 @@ contract GambleTool {
     return bets[index]._maxPlayers - bets[index].players.length;
   }
 
-  function startGame(uint index) public returns (uint) { //Not going to return
+  function ManualStartGame(uint index) public returns (address) { 
     require(msg.sender == owner);
+    return startGame(index);
+  }
+  
+  function startGame(uint index) internal returns (address) { //Not going to return
     require(bets[index].open);
     bets[index].open = false;
     uint playerCount = bets[index].players.length;
     uint winningPlayer = flip(bets[index].players.length);
+    address winningAddress = bets[index].players[winningPlayer];
     uint ethPool = (bets[index]._betValue*playerCount*90)/100; //They win 90% of total pool.
     bets[index].players[winningPlayer].transfer(ethPool);
-    win(winningPlayer, ethPool);
+    win(index, winningAddress, ethPool);
     bets[index].winner = bets[index].players[winningPlayer];
-    return winningPlayer;
+    return winningAddress;
   }
 
   function flip(uint playerCount) internal view returns (uint) { //needs to be bool
